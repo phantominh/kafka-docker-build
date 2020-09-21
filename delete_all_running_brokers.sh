4a49055a02b8252c0f9449243d690a04bd1a8c69
minhtp2 <minhtp2@illinois.edu>
2020-09-21 16:50:21 +0700

    Final Update

---
+++ delete_all_running_brokers.sh
@@ -0,0 +1,2 @@
+sudo docker stop $(sudo docker ps -q)
+sudo docker container prune
--- start_sasl_scram_cluster.sh
+++ start_sasl_scram_cluster.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+export IP_ADDRESS=192.168.1.135
 export KAFKA_SSL_SECRETS_DIR=$PWD/secrets
 export KAFKA_SASL_SCRAM_SECRETS_DIR=$PWD/sasl-scram/secrets
 cd $PWD/sasl-scram/
-docker-compose -f docker-compose-scram.yml up -d
+sudo -E docker-compose -f docker-compose-scram.yml up -d
