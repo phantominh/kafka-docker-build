612d85bf6afd98329aeef1ad49f714955355aa38
Minh Phan <minhtp2@illinois.edu>
2020-09-29 10:26:05 +0700

    Update

---
+++ start-broker.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export BROKER_IP_ADDRESS=192.168.1.112
+export ZOO_IP_ADDRESS=192.168.1.135
+export KAFKA_SSL_SECRETS_DIR=$PWD/secrets
+export KAFKA_SASL_SCRAM_SECRETS_DIR=$PWD/sasl-scram/secrets
+cd $PWD/sasl-scram/
+sudo -E docker-compose -f broker.yml up -d
